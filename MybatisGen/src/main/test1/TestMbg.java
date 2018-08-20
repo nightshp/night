e882c34b98d29349340300dde391d95854cafbc6
walkershp <1969307962@qq.com>
2018-08-20 09:55:05 +0800

    逆向工程

---
+++ MybatisGen/src/main/test1/TestMbg.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+public class TestMbg {
+
+    public static void main(String[] args) throws Exception {
+        // MBG 执行过程中的警告信息
+        List<String> warnings = new ArrayList<String>();
+        // 当生成的代码重复时，覆盖原代码
+        boolean overwrite = true;
+        // 读取MBG配置文件
+        InputStream is = TestMbg.class.getClassLoader().getResourceAsStream("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(is);
+        is.close();
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        // 创建 MBG
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        // 执行生成代码
+        myBatisGenerator.generate(null);
+        // 输出警告信息
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
+
+    //测试mbg生成的代码
+    public void testMbg() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+}
